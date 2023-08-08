d3853a5790607e186f4c7af3f5b2d85af4f9fe8a
maulvi-zm <134979616+maulvi-zm@users.noreply.github.com>
2023-08-08 20:45:24 +0700

    Time: 7 ms (99.4%), Space: 13.6 MB (7.3%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        stack = []
+
+        for i in s:
+            if i in "({[":
+                stack.append(i)
+            else:
+                if (not stack or 
+                    (i == ")" and stack[-1] != "(") or 
+                    (i == "}" and stack[-1] != "{") or 
+                    (i == "]" and stack[-1] != "[")):
+                    return False
+                
+                stack.pop()
+        
+        return not stack
+            
