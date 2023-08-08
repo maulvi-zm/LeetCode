8e5c263d84c0587df7cdea1c29bb37d42739f900
maulvi-zm <134979616+maulvi-zm@users.noreply.github.com>
2023-08-08 22:34:14 +0700

    Time: 72 ms (82.23%), Space: 14.4 MB (83.17%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -4,24 +4,25 @@ class Solution(object):
         :type asteroids: List[int]
         :rtype: List[int]
         """
+        
+        stack = []
+
         i = 0 
 
-        while i < len(asteroids)-1:
-            if asteroids[i] > 0 and asteroids[i+1] < 0:
-                if abs(asteroids[i+1]) > asteroids[i]:
-                    asteroids.pop(i)
-                    i -= 2
-                elif abs(asteroids[i+1]) == asteroids[i]:
-                    asteroids.pop(i+1)
-                    asteroids.pop(i)
-                    i -= 2
-                else:
-                    asteroids.pop(i+1)
-                    i -= 1
-            
-            i += 1
+        while i < len(asteroids):
+            if asteroids[i] > 0:
+                stack.append(asteroids[i])
+            else:
+                while stack and -1 * asteroids[i] > stack[-1] and stack[-1] > 0:
+                    stack.pop()
+                
+                if not stack:
+                    stack.append(asteroids[i])
+                elif stack[-1] == -1 * asteroids[i]:
+                    stack.pop()
+                elif stack[-1] < 0:
+                    stack.append(asteroids[i])
 
-            if i < 0:
-                i = 0 
+            i += 1
         
-        return asteroids
+        return stack
