7180f825bb16c17d6654307ff3ec7a710c78507a
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2024-09-10 11:11:13 +0700

    Time: 309 ms (32.06%), Space: 155.6 MB (6.57%) - LeetHub

---
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -0,0 +1,39 @@
+class NumMatrix {
+    private:
+        vector<vector<int>> prefix;
+    public:
+        NumMatrix(vector<vector<int>>& matrix) {
+
+            for (int i = 0; i < matrix.size(); i++){
+                vector<int> row;
+                int temp = 0;
+
+                for (int j = 0; j < matrix[i].size(); j++){
+                    temp += matrix[i][j];
+                    row.push_back(temp);
+                }
+
+                prefix.push_back(row);
+            }
+
+            for (int i = 1; i < matrix.size(); i++){
+                for (int j = 0; j < matrix[i].size(); j++){
+                    prefix[i][j] += prefix[i-1][j];
+                }
+            }
+        }
+        
+        int sumRegion(int row1, int col1, int row2, int col2) {
+            int left_down = row1 > 0 ? prefix[row1-1][col2] : 0;
+            int right_up = col1 > 0 ? prefix[row2][col1-1] : 0;
+            int corner = (row1 > 0 && col1 > 0) ? prefix[row1-1][col1-1] : 0;
+
+            return prefix[row2][col2] - left_down - right_up + corner;
+        }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
