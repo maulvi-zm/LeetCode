8394a517085d4bc8caeaf74afd62315e831c1a16
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2025-11-02 20:15:02 +0700

    0078-SUBSETS Stats: Time: 0 ms (100%), Space: 9.8 MB (94.66%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     void create_subsets(vector<vector<int>>& res, vector<int>& nums, int index,
-                        vector<int> current) {
+                        vector<int>& current) {
+        res.push_back(current);
         for (int i = index; i < nums.size(); i++) {
-            vector<int> copy = current;
-            copy.push_back(nums[i]);
-            res.push_back(copy);
-
-            create_subsets(res, nums, i + 1, copy);
+            current.push_back(nums[i]);
+            create_subsets(res, nums, i + 1, current);
+            current.pop_back();
         }
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res = {};
-        res.push_back(vector<int>{});
-        create_subsets(res, nums, 0, vector<int>{});
+        vector<int> curr;
+        create_subsets(res, nums, 0, curr);
 
         return res;
     }
