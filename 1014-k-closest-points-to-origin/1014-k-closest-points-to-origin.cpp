2946ba5a989213b8984367480926c96903c96773
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2025-11-04 11:30:18 +0700

    1014-K-CLOSEST-POINTS-TO-ORIGIN Stats: Time: 47 ms (56.11%), Space: 74.4 MB (47.18%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-  vector<vector<int>> kClosest(vector<vector<int>> &points, int k) {
-    priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> heap;
-    for (int i = 0; i < points.size(); i++) {
-      auto point = points[i];
-      auto distance = sqrt(pow(point[0], 2) + pow(point[1], 2));
-      heap.push({distance, i});
-    }
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<double, int>, vector<pair<double, int>>> heap;
+        for (int i = 0; i < points.size(); i++) {
+            auto point = points[i];
+            auto distance = sqrt(pow(point[0], 2) + pow(point[1], 2));
+            heap.push({distance, i});
 
-    vector<vector<int>> res;
-    while (k--) {
-      int i = heap.top().second;
-      res.push_back(points[i]);
-      heap.pop();
-    }
+            if (heap.size() > k) {
+                heap.pop();
+            }
+        }
 
-    return res;
-}
+        vector<vector<int>> res;
+        while (k--) {
+            res.push_back(points[heap.top().second]);
+            heap.pop();
+        }
+
+        return res;
+    }
 };
