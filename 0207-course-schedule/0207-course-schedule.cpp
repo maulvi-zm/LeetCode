edf2169d520038c3fcd20043356da912de5477b8
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2025-11-07 21:07:27 +0700

    0207-COURSE-SCHEDULE Stats: Time: 3 ms (79.28%), Space: 19.3 MB (73.45%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,55 +1,34 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, pair<unordered_set<int>, unordered_set<int>>> m;
-        vector<bool> visited(numCourses, false);
-        queue<int> q;
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
 
-        // Find the start point
-        for (auto& relation : prerequisites){
-            if (m.count(relation[1]) == 0){
-                m.insert({relation[1], {unordered_set<int>(), unordered_set<int>()}});
-            }
-
-            if (m.count(relation[0]) == 0){
-                m.insert({relation[0], {unordered_set<int>(), unordered_set<int>()}});
-            }
-
-            // It can be used to finish a course
-            m[relation[1]].first.insert(relation[0]);
-
-            // The target course need this as prereq
-            m[relation[0]].second.insert(relation[1]);
+        for (auto& pre: prerequisites){
+            int course = pre[0];
+            int prereq = pre[1];
+            
+            graph[prereq].push_back(course);
+            indegree[course]++;
         }
 
-        // Add the start point to the queue
+        queue<int> q;
         for (int i = 0; i < numCourses; i++){
-            if (m[i].second.size() == 0){
+            if (indegree[i] == 0){
                 q.push(i);
             }
         }
 
-        // No start point
-        if (q.empty()){
-            return false;
-        }
-
         int completed = 0;
         while (!q.empty()){
-            int curr = q.front();
-            q.pop();
+            int curr = q.front(); q.pop();
             completed++;
 
-            // Loop towards the next course that can be completed
-            for (auto& neighbor: m[curr].first){
-                // If this node is completed, then all next node needs to remove the prereq 
-                if (m[curr].second.size() == 0){
-                    m[neighbor].second.erase(curr);
-                }
+            for (auto& neighbor: graph[curr]){
+                indegree[neighbor]--;
 
-                if (m[neighbor].second.size() == 0){
+                if (indegree[neighbor] == 0){
                     q.push(neighbor);
-                    visited[neighbor] = true;
                 }
             }
         }
