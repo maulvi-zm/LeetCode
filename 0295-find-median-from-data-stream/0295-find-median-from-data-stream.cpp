dbfba69177d057a3523a6471696ae933cde4b52c
Maulvi Ziadinda Maulana <13522122@std.stei.itb.ac.id>
2025-11-23 06:31:45 +0700

    0295-FIND-MEDIAN-FROM-DATA-STREAM Stats: Time: 107 ms (14.04%), Space: 148.5 MB (61.39%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -2,60 +2,28 @@ class MedianFinder {
 private:
     priority_queue<int> low;
     priority_queue<int, vector<int>, greater<int>> high;
+
 public:
-    MedianFinder() {
-    }
-    
+    MedianFinder() {}
+
     void addNum(int num) {
-        // Requirements:
-        // 1. The top of the left must be < top of the right
-        // 2. The left length must be equal to the right or at most + 1
+        low.push(num);
 
-        // If its empyt or odd then add to the left
-        if (low.empty() && high.empty()){
-            low.push(num);
-            return;
-        }
+        high.push(low.top());
+        low.pop();
 
-        // If one of them is empty, there is an impossible cond: left is empty and right is filled, because left will be filled first
-        if (high.empty()){
-            if (low.top() > num){
-                high.push(low.top());
-                low.pop();
-                low.push(num);
-            } else {
-                high.push(num);
-            }
-
-            return;
-        }
-
-        // If neither of them is empty
-        if (low.size() > high.size()){ // need to fill the right
-            if (low.top() > num){
-                high.push(low.top());
-                low.pop();
-                low.push(num);
-            } else {
-                high.push(num);
-            }
-        } else { // need to fill the left
-            if (high.top() < num){
-                low.push(high.top());
-                high.pop();
-                high.push(num);
-            } else {
-                low.push(num);
-            }
+        if (low.size() < high.size()) {
+            low.push(high.top());
+            high.pop();
         }
     }
-    
+
     double findMedian() {
-        if ((low.size() + high.size()) % 2 != 0){
+        if (low.size() > high.size()) {
             return low.top();
         }
 
-        return  ((double) (low.top()) + (double) (high.top())) / 2;
+        return (low.top() + high.top()) / 2.0;
     }
 };
 
